907b4c92688e932de0faef64f751435a47abbb64
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-20 11:39:23 +0100

    ok

--- SocketClient/Program.cs
+++ SocketClient/Program.cs
@@ -9,6 +9,34 @@ namespace SocketClient
         static void Main(string[] args)
         {
             Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            string strIpAddress = "", strPort = ""; // Leggono da tastiera
+            IPAddress ipAddress = null;
+            int nPort;
+
+            try
+            {
+                Console.WriteLine("IP del server: ");
+                strIpAddress = Console.ReadLine();
+
+                Console.WriteLine("Porta del server: ");
+                strPort = Console.ReadLine();
+
+                /*(TryParse)Prova a copiare lstringa alla variabile "ipAddress"*/
+                if (!IPAddress.TryParse(strIpAddress.Trim(), out ipAddress)) // True se ci riesce
+                {
+                    Console.WriteLine("IP non valido.");
+                    return;
+                }
+                if (!int.TryParse(strPort, out nPort))
+                {
+                    Console.WriteLine("Porta non valida");
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
